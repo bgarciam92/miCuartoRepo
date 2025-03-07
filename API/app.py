15aeb098623018b9d3c6788e5a5e2a0bb529778e
Brayand Garcia <bgarciam92@gmail.com>
2025-03-06 19:59:56 -0600

    metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -14,3 +14,9 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows = ["Docker", "Bash", "Linux", "Ingles", "Python", "Javascript", "Azure", "Cyborg"]
+    return rows   
